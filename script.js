845d6c7222a9b6f43ae909716f40c931ba9e44d9
ajoxales <ajoxales@gmail.com>
2023-06-30 21:48:08 +0800

    added calculate func

---
+++ script.js
@@ -0,0 +1,9 @@
+let ipAdd = 0;
+let prefix = 0;
+
+function calculate() {
+  ipAdd = document.getElementById("ip-address").value;
+  prefix = document.getElementById("prefix").value;
+  console.log(ipAdd);
+  console.log(prefix);
+}
