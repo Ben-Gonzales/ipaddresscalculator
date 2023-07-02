55fbfbc4c8fcea82a0c78273423e79397973286e
ajoxales <ajoxales@gmail.com>
2023-07-02 19:27:32 +0800

    commented errors

--- script.js
+++ script.js
@@ -113,9 +113,8 @@ function calculate() {
 
     // const networkAddress = getNetworkAddress(ipAddress, prefix);
     // const broadcast = getBroadcastAddress(ipAddress, prefix);
-  }
 
-  /*function Interval(prefix){
+    /*function Interval(prefix){
   // Calculate the interval based on the prefix and octet position
   const interval = Math.pow(2, 8 - (prefix % 8 || 8));
   return interval;
@@ -155,23 +154,24 @@ function getNextNetworkAddress(ipAddress, prefix) {
   return nextNetworkAddress;
 }*/
 
-  const networkAddress = getNetworkAddress(ipAddress, prefix);
-  // const broadcast = getBroadcastAddress(ipAddress, prefix);
-  // const nextNetworkAddress = getNextNetworkAddress(ipAddress, prefix);
-  // const Interval = Interval(prefix);
+    const networkAddress = getNetworkAddress(ipAddress, prefix);
+    // const broadcast = getBroadcastAddress(ipAddress, prefix);
+    // const nextNetworkAddress = getNextNetworkAddress(ipAddress, prefix);
+    // const Interval = Interval(prefix);
 
-  console.log(networkAddress);
-  console.log(networkAddress);
-  // console.log(nextNetworkAddress);
-  //console.log(Interval);
+    console.log(networkAddress);
+    console.log(networkAddress);
+    // console.log(nextNetworkAddress);
+    //console.log(Interval);
 
-  // Update table values
-  document.querySelector("#network-address").textContent = networkAddress;
-  document.querySelector("#lowest-usable").textContent = lowestUsable;
-  document.querySelector("#highest-usable").textContent = highestUsable;
-  document.querySelector("#broadcast-address").textContent = broadcastAddress;
-  document.querySelector("#next-network-address").textContent =
-    nextNetworkAddress;
+    // Update table values
+    document.querySelector("#network-address").textContent = networkAddress;
+    // document.querySelector("#lowest-usable").textContent = lowestUsable;
+    // document.querySelector("#highest-usable").textContent = highestUsable;
+    // document.querySelector("#broadcast-address").textContent = broadcastAddress;
+    // document.querySelector("#next-network-address").textContent =
+    //   nextNetworkAddress;
+  }
 
   // For Alex
   function getSubnetMask(hostBits) {
